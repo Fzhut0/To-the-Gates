7a631d1ec7452dfebc1c63ed767b91ccc54c4404
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2021-06-22 16:30:29 +0200

    Added addition VFX for mouseover/after tower placement

--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class PlaceTower : MonoBehaviour
 {
     [SerializeField] GameObject towerPrefab;
+    [SerializeField] GameObject placedPrefab;
+    [SerializeField] Material originalMaterial;
+    [SerializeField] Material overMaterial;
+
+    MeshRenderer mRenderer;
+    [SerializeField] bool isPlaceable = true;
 
 
+    private void Start()
+    {
+        mRenderer = GetComponent<MeshRenderer>();
+        originalMaterial = mRenderer.material;
+    }
 
     private void OnMouseDown()
     {
-
-        Instantiate(towerPrefab.gameObject, transform.position, Quaternion.identity);
+        PutTower();
     }
 
+    void OnMouseOver()
+    {
+        mRenderer.material = overMaterial;
+    }
 
+    private void OnMouseExit()
+    {
+        mRenderer.material = originalMaterial;
+    }
+
+    void PutTower()
+    {
+        if (isPlaceable == true)
+        {
+            Instantiate(towerPrefab.gameObject, transform.position, Quaternion.identity);
+            Instantiate(placedPrefab.gameObject, transform.position, Quaternion.identity);
+        }
+        isPlaceable = false;
+    }
 
 }
