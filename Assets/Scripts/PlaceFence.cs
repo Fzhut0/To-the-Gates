f6d915605239118706411e6f17616d4d82ab9867
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-01-07 20:35:13 +0100

    Created fence placement on path mechanic

---
+++ Assets/Scripts/PlaceFence.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceFence : MonoBehaviour
+{
+
+    [SerializeField] GameObject fencePrefab;
+    [SerializeField] int cost = 50;
+
+    [SerializeField] bool fencePlaceableLeft = true;
+    [SerializeField] bool fencePlaceableFw = true;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        PutFence();
+    }
+
+    void PutFence()
+    {
+        ResourcesBank bank = FindObjectOfType<ResourcesBank>();
+        if (fencePlaceableLeft == true && bank.CurrentBalance >= cost)
+        {
+            Instantiate(fencePrefab.gameObject, transform.position, Quaternion.identity);
+            bank.Withdraw(cost);
+        }
+        fencePlaceableLeft = false;
+
+        if (fencePlaceableFw == true && bank.CurrentBalance >= cost)
+        {
+            Instantiate(fencePrefab.gameObject, transform.position, Quaternion.LookRotation(Vector3.right));
+            bank.Withdraw(cost);
+        }
+        fencePlaceableFw = false;
+    }
+}
+
